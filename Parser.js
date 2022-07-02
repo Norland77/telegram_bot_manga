275efe77e4c98a2160b04e5acb5c74a65e028602
Norland <nikitafoka30@gmail.com>
2022-07-02 12:16:49 +0300

    init

---
+++ Parser.js
@@ -0,0 +1,47 @@
+const puppeteer = require('puppeteer');
+function sleep(ms) {
+    ms += new Date().getTime();
+    while (new Date() < ms){}
+} 
+    const Start = async (text) => {
+    console.log("Opening the browser......");
+    browser = await puppeteer.launch({
+        dumpio: false,
+        headless: true,
+        args: [
+            '--disable-setuid-sandbox',
+            '--no-sandbox',
+        ],
+        waitForInitialPage: true,
+    });
+    let page = await browser.newPage();
+    console.log(`Navigating to ${text}...`);
+    await page.goto(text, {timeout: 0});
+    await page.screenshot({                      
+        path: "./screenshot.png",                   
+        fullPage: true                     
+    });
+    //await page.waitForSelector('.page__inner');
+    await page.waitForSelector('.main-wrapper');
+    let picture = await page.$$eval('.media-sidebar__cover', pictures => {
+        pictures = pictures.map(el => el.querySelector('img').src)
+        return pictures;
+    });
+    let title = await page.$$eval('.media-name__body', titles => {
+        titles = titles.map(el => el.querySelector('.media-name__alt').innerText)
+        return titles;
+    });
+    let urls = await page.$$eval('.vue-recycle-scroller__item-view', links => {
+        links = links.filter(link => link.querySelector('.media-chapter_new'))
+        links = links.map(el => el.querySelector('.media-chapter__name > a').href)
+        return links;
+    });
+    await browser.close();
+    console.log("Closing the browser......");
+    return {
+        url: urls,
+        title: title,
+        picture: picture,
+    }
+} 
+module.exports = Start;
---
+++ index.js
@@ -0,0 +1,29 @@
+const parser = require('./Parser')
+const token = '5496733880:AAGUMzcZMRlrznEzpiJIwN1tZufRCCb5X-I'
+const telegramApi = require('node-telegram-bot-api');
+const bot = new telegramApi(token, {polling: true});
+
+function sleep(ms) {
+    ms += new Date().getTime();
+    while (new Date() < ms){}
+} 
+async function Parsing(text, chatID) {
+    while(true) {
+        let charpet = await parser(text);
+        bot.sendPhoto(chatID, `${charpet.picture}`)
+        bot.sendMessage(chatID, `New charpet ${charpet.title}: ${charpet.url}`);
+        sleep(30000)
+    }
+}
+
+bot.on('message', msg => {
+    let text = msg.text;
+    const https = 'https://mangalib.me/';
+    let result = text.match(https);
+    const chatID = msg.chat.id;
+    if (result) {
+        Parsing(text, chatID);
+    } else {
+        bot.sendMessage(chatID, `${text} is dont link`);
+    }
+})
